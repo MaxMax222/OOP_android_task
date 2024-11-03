0d2e645f59dceaea55921a245e10fe34780a5f10
Maxim Zyuryaev <zrv.maxim@gmail.com>
2024-11-03 23:40:51 +0200

    added logic for bird and fish activities

--- OOP-learn/BirdActivity.cs
+++ OOP-learn/BirdActivity.cs
@@ -10,19 +10,59 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using static Java.Util.Jar.Attributes;
 
 namespace OOP_learn
 {
 	[Activity (Label = "BirdActivity")]			
 	public class BirdActivity : Activity
 	{
-		protected override void OnCreate (Bundle savedInstanceState)
+        Animal bird;
+        Button ret, talk, walk;
+        TextView name;
+        protected override void OnCreate (Bundle savedInstanceState)
 		{
 			base.OnCreate (savedInstanceState);
 
 			// Create your application here
 			SetContentView(Resource.Layout.Bird);
-		}
-	}
+            Init();
+            AddClicks();
+        }
+
+        private void AddClicks()
+        {
+            ret.Click += Ret_Click;
+            talk.Click += Talk_Click;
+            walk.Click += Walk_Click;
+        }
+
+        private void Walk_Click(object sender, EventArgs e)
+        {
+            Toast.MakeText(this, bird.Move(), ToastLength.Short).Show();
+        }
+
+        private void Talk_Click(object sender, EventArgs e)
+        {
+            Toast.MakeText(this, bird.Talk(), ToastLength.Short).Show();
+        }
+
+        private void Ret_Click(object sender, EventArgs e)
+        {
+            Finish();
+        }
+
+        private void Init()
+        {
+            bird = new Bird("Sigma Crow", Animal.Genders.Male, 1000);
+
+            ret = FindViewById<Button>(Resource.Id.ret);
+            talk = FindViewById<Button>(Resource.Id.talkBtn);
+            walk = FindViewById<Button>(Resource.Id.moveBtn);
+            name = FindViewById<TextView>(Resource.Id.nameTxt);
+
+            name.Text += bird.Name;
+        }
+    }
 }
 
--- OOP-learn/FishActivity.cs
+++ OOP-learn/FishActivity.cs
@@ -16,13 +16,52 @@ namespace OOP_learn
 	[Activity (Label = "FishActivity")]			
 	public class FishActivity : Activity
 	{
+		Animal fish;
+		Button ret, talk, walk;
+		TextView name;
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
 			base.OnCreate (savedInstanceState);
 
 			// Create your application here
 			SetContentView(Resource.Layout.FIsh);
+			Init();
+			AddClicks();
 		}
-	}
+
+        private void AddClicks()
+        {
+            ret.Click += Ret_Click;
+            talk.Click += Talk_Click;
+            walk.Click += Walk_Click;
+        }
+
+        private void Walk_Click(object sender, EventArgs e)
+        {
+            Toast.MakeText(this, fish.Move(), ToastLength.Short).Show();
+        }
+
+        private void Talk_Click(object sender, EventArgs e)
+        {
+			Toast.MakeText(this, fish.Talk(), ToastLength.Short).Show();
+        }
+
+        private void Ret_Click(object sender, EventArgs e)
+        {
+			Finish();
+        }
+
+        private void Init()
+        {
+            fish = new Fish("Blob", Animal.Genders.Male, 1000);
+
+			ret = FindViewById<Button>(Resource.Id.ret);
+			talk = FindViewById<Button>(Resource.Id.talkBtn);
+			walk = FindViewById<Button>(Resource.Id.moveBtn);
+			name = FindViewById<TextView>(Resource.Id.nameTxt);
+
+			name.Text += fish.Name;
+        }
+    }
 }
 
